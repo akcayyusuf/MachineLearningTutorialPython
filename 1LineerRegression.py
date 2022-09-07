f3113799a3ea05d8e7f71a6133df9ed4ab46b523
z004e6un <z004e6un@MD3ICTKC>
2022-09-07 08:50:12 +0300

    JOBLİB Ajoblib and pickle

--- 1LineerRegression.py
+++ 1LineerRegression.py
@@ -14,6 +14,23 @@ reg.fit(df[['area']],df.price)
 
 plt.plot(df.area,reg.predict(df[['area']]),color='blue' )
 
-
 plt.show()
+
+import pickle
+with open('model_pickle','wb') as f:
+    pickle.dump(reg,f)
+
+with open('model_pickle','rb') as f:
+    savedModel=pickle.load(f)
+print(reg.predict([[5000]]))
+print(savedModel.predict([[5000]]))
+
+
+from sklearn.externals import joblib
+
+joblib.dump(reg,'model_joblib')
+
+mj=joblib.load('model_joblib')
+
+print(mj.predict([5000]))
 
